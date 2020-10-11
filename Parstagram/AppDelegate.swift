b3608d97b2394b8c9f7d6806c8efb77e0fb49adf
Gary Feng <garyfeng961@gmail.com>
2020-10-10 20:17:00 -0400

    Parstagram

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "fVzuEClTn7kEZ8ig7FKinQHSqOj44wB8QDEcS2We"
+                    $0.clientKey = "0cvvffmHJXMBUoC88UIneTyqW9EoAfS1YB0tYqS7"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
